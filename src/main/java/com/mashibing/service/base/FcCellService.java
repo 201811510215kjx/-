976d51ab5510a2dcef444426201dd4f63b871397
kejx <947002886@qq.com>
2021-09-14 09:22:44 +0800

    第一次提交项目

---
+++ src/main/java/com/mashibing/service/base/FcCellService.java
@@ -0,0 +1,16 @@
+package com.mashibing.service.base;
+
+import com.mashibing.bean.FcCell;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 房间信息表 服务类
+ * </p>
+ *
+ * @author lian
+ * @since 2021-07-22
+ */
+public interface FcCellService extends IService<FcCell> {
+
+}
---
+++ src/main/java/com/mashibing/service/base/TblEnvirSettingService.java
@@ -0,0 +1,16 @@
+package com.mashibing.service.base;
+
+import com.mashibing.bean.TblEnvirSetting;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 环境配置 服务类
+ * </p>
+ *
+ * @author lian
+ * @since 2021-07-22
+ */
+public interface TblEnvirSettingService extends IService<TblEnvirSetting> {
+
+}
